3ed238b5406244a7c67faa20f4bfc3bd38966d7d
Dang Duy Hoang <dangduyhoang3009@gmail.com>
2017-01-09 02:00:18 +0700

    init project

---
+++ src/main/java/com/hoang/spring/SpringStudyApplication.java
@@ -0,0 +1,12 @@
+package com.hoang.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudyApplication.class, args);
+	}
+}
